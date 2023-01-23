94f3eae56ed9c80f1df068bf5c346373a2a6cf83
Renzo <21077703@student.hhs.nl>
2023-01-23 14:03:17 +0100

    fix the actual base bug lol

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [svelte(), mkcert()],
-  // base path for gitlab pages
-  base: "/id-2223-s1/moed/moed/",
+  base: "/id-2223-s1/moed/uplift/",
   server: {
     host: "127.0.0.1",
   },
