057e74d5a9bd53acf9d36293b11d9bd247152098
Renzo <21077703@student.hhs.nl>
2023-03-15 21:16:08 +0100

    Updated factory service

--- src/lib/client/services/FactoryService.ts
+++ src/lib/client/services/FactoryService.ts
@@ -7,14 +7,22 @@ import { request as __request } from "../core/request";
 
 export class FactoryService {
   /**
-   * Factory
+   * Factory to create dummy data
    * @returns any Successful Response
    * @throws ApiError
    */
-  public static factory(): CancelablePromise<any> {
+  public static factory(): CancelablePromise<{
+    message: string;
+    status: number;
+  }> {
     return __request(OpenAPI, {
       method: "POST",
       url: "/api/factory/",
+      errors: {
+        400: `Bad Request`,
+        404: `Not Found`,
+        500: `Internal Server Error`,
+      },
     });
   }
 }
