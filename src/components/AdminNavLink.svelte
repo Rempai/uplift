9321f14ed679e03a0a252730cf78b0f2a08fac03
JFK <jasrajkhedoe@outlook.com>
2023-03-16 14:43:31 +0100

    removed h prop from div

--- src/components/AdminNavLink.svelte
+++ src/components/AdminNavLink.svelte
@@ -16,7 +16,7 @@
 <button
   on:click={() => push(link)}
   class={path === link ? `bg-night-1 ${link_class}` : `${link_class}`}>
-  <div class="text-frost-4 w-6 h-5 mr-2">
+  <div class="text-frost-4 w-6 mr-2">
     <slot />
   </div>
   <p>{name}</p>
