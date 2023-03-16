c15629ea6017b85c97619221eb8d97d4f6156932
JFK <jasrajkhedoe@outlook.com>
2023-03-16 11:00:46 +0100

    added unplugin-icon config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
+import Icons from "unplugin-icons/vite";
+
 import mkcert from "vite-plugin-mkcert";
 import path from "path";
 
 export default defineConfig({
-  plugins: [svelte(), mkcert()],
+  plugins: [svelte(), mkcert(),  Icons({
+    compiler: "svelte",
+    autoInstall: true,
+    iconCustomizer(collection, icon, props) {
+      props.width = "1em";
+      props.height = "1em";
+    },
+  }),],
   base: "/id-2223-s1/moed/uplift/",
   root: "./",
   publicDir: "public",
