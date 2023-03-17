d5bfdc63173e3cce9690ee3ca09b1a831536b487
Renzo <21077703@student.hhs.nl>
2023-03-17 14:58:50 +0000

    Update Button.svelte

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -8,7 +8,7 @@
   let classes = `${$$props.class} rounded text-2xl text-center cursor-pointer shadow no-underline transition text-storm-3 hover:text-storm-3 hover:brightness-110 px-1 py-2 md:px-5 md:py-2 border-2 border-transparent`;
 
   if ($$slots.icon) {
-    classes = classes + ` flex items-center `;
+    classes = classes + ` flex items-center`;
   }
 </script>
 
