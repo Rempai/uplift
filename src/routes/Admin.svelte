918e9104376d3a6ff97c4ff9fd6684970bef17ec
Renzo <21077703@student.hhs.nl>
2023-03-29 16:19:54 +0200

    Added checkaccount check, removed old code

--- src/routes/Admin.svelte
+++ src/routes/Admin.svelte
@@ -1,30 +1,14 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import { location, push } from "svelte-spa-router";
 
   import { checkAccount } from "@/lib/adminLogic";
-  import { FactoryService } from "@/lib/client";
 
-  import Button from "@/components/Button.svelte";
   import AdminNav from "@/components/AdminNav.svelte";
-  import Modal from "@/components/Modal.svelte";
 
-  import RealAnalysis1 from "/real-analysis1.png";
-  import RealAnalysis2 from "/real-analysis2.png";
-
-  checkAccount();
-
-  let showModal = false;
-
-  const resetDatabase = async () => {
-    await FactoryService.factory().then(() => {
-      localStorage.clear();
-      push("/");
-    });
-  };
-
-  onMount(() => {
-    if ($location === "/admin/database") showModal = true;
+  let check = false;
+  onMount(async () => {
+    await checkAccount();
+    check = true;
   });
 </script>
 
@@ -32,45 +16,32 @@
   <title>Uplift | admin</title>
 </svelte:head>
 
-<Modal {showModal} modalHeader="Database Options">
-  <p>If you want to reset the database to factory data, click `Reset Database`.</p>
-  <p><b>Note: All current data will be lost.</b></p>
-  <div class="flex gap-4 mt-3">
-    <Button
-      onClick={resetDatabase}
-      text="Reset Database"
-      class="bg-transparent !border-aurora-red hover:bg-aurora-red" />
-    <Button
-      onClick={() => history.back()}
-      text="Go back"
-      class="bg-transparent !border-aurora-green hover:bg-aurora-green" />
-  </div>
-</Modal>
+{#if check}
+  <AdminNav />
 
-<AdminNav />
-
-<main class="admin-space">
-  <h1>Admin Page</h1>
-  <hr class="mb-3" />
-  <div class="flex justify-between w-full px-3 gap-4">
-    <div class="card w-full">
-      <div class="flex justify-between items-center">
-        <h2>Logs</h2>
-      </div>
-      <hr class="mb-3" />
-      <div class="overflow-y-auto bg-night-1 rounded p-4 h-screen" />
-    </div>
-    <div class="flex flex-col gap-4 w-full">
-      <div class="card">
-        <h2>Happy users</h2>
+  <main class="admin-space">
+    <h1>Admin Page</h1>
+    <hr class="mb-3" />
+    <div class="flex justify-between w-full px-3 gap-4">
+      <div class="card w-full">
+        <div class="flex justify-between items-center">
+          <h2>Logs</h2>
+        </div>
         <hr class="mb-3" />
-        <img class="w-full rounded" src={RealAnalysis1} alt="Happy Users" />
+        <div class="overflow-y-auto bg-night-1 rounded p-4 h-screen" />
       </div>
-      <div class="card">
-        <h2>Stock value</h2>
-        <hr class="mb-3" />
-        <img class="w-full rounded" src={RealAnalysis2} alt="Stock Value" />
+      <div class="flex flex-col gap-4 w-full">
+        <div class="card">
+          <h2>Happy users</h2>
+          <hr class="mb-3" />
+          <img class="w-full rounded" src="real-analysis1.png" alt="Happy Users" />
+        </div>
+        <div class="card">
+          <h2>Stock value</h2>
+          <hr class="mb-3" />
+          <img class="w-full rounded" src="real-analysis2.png" alt="Stock Value" />
+        </div>
       </div>
     </div>
-  </div>
-</main>
+  </main>
+{/if}
