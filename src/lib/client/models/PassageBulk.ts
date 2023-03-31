6a8bb4aabc6401b8190cda2395bfe53ec9f34bb0
Renzo <21077703@student.hhs.nl>
2023-03-31 19:48:54 +0200

    update bulk call

--- src/lib/client/models/PassageBulk.ts
+++ src/lib/client/models/PassageBulk.ts
@@ -2,13 +2,4 @@
 /* tslint:disable */
 /* eslint-disable */
 
-export type PassageBulk = {
-  passage?: string;
-  content?: string;
-  branch?: string;
-  continueButton?: boolean;
-  speaker?: string;
-  trunk?: boolean;
-  attributeId?: number;
-  rideId?: number;
-};
+export type PassageBulk = Array<any>;
