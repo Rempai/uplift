f786e482ac4500992b9b11f122b936f294846141
JFK <jasrajkhedoe@outlook.com>
2023-04-20 16:46:56 +0200

    added achievement modal

---
+++ src/components/Achievement.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  let achievementTitle: string;
+  let achievementDescription: string;
+  let achieved: boolean;
+  let IconImage: any;
+</script>
+
+<!-- Modal -->
+<div class="w-80 h-20">
+  <div id="background" class="bg-white rounded h-full">
+    <div
+    id="border"
+      class="border-8 border-storm-1 rounded outline outline-8 outline-offset-0 outline-aurora-purple flex h-full" />
+  </div>
+</div>
