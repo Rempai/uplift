ff3a5f342154eee26106c68c9d346c3bf1c096b7
luuks1609 <luuks1609@gmail.com>
2023-05-03 14:06:11 +0200

    positioning skissue

--- src/components/Multimedia.svelte
+++ src/components/Multimedia.svelte
@@ -294,73 +294,74 @@
   {/if}
 </Modal>
 
-<div
-  class="bg-night-1 h-[8.68em] w-[13.1em] fixed z-10 bottom-[2.87em] left-1/2 transform -translate-x-1/2 rounded-md flex flex-row">
-  <div
-    class="flex flex-col items-center justify-evenly w-12 bg-white/10 mr-2"
-    style="border-radius: 6px">
-    <img
-      src="multimedia/Home_icon.png"
-      alt="info"
-      class="w-6 h-6 cursor-pointer"
-      on:click={() => forward(-1, "Home")}
-      on:keydown />
-    <img
-      src="multimedia/Info_Icon.png"
-      alt="info"
-      class="w-6 h-6 cursor-pointer"
-      on:click={() => forward(2, "Info")}
-      on:keydown />
-    <img
-      src={dialogIconSrc}
-      alt="info"
-      class="w-6 h-6 cursor-pointer"
-      on:click={dialog}
-      on:keydown />
-  </div>
-
-  <!-- this section -->
-  <div class="flex flex-col flex-1 justify-center">
-    <div class="flex gap-2 p-2">
+<div class="absolute inset-0 flex justify-center items-center top-[60%] z-10">
+  <div class="bg-night-1 h-[8.68em] w-[13.1em] rounded-md flex flex-row">
+    <div
+      class="flex flex-col items-center justify-evenly w-12 bg-white/10 mr-2"
+      style="border-radius: 6px">
       <img
-        src="multimedia/Achievements_icon.png"
-        alt="achievements"
-        class="w-full h-full cursor-pointer"
-        on:click={() => forward(1, "Achievements")}
+        src="multimedia/Home_icon.png"
+        alt="info"
+        class="w-6 h-6 cursor-pointer"
+        on:click={() => forward(-1, "Home")}
         on:keydown />
       <img
-        src="multimedia/Contacts_icon.png"
-        alt="contacts"
-        class="w-full h-full cursor-pointer"
-        on:click={() => forward(0, "Contacts")}
+        src="multimedia/Info_Icon.png"
+        alt="info"
+        class="w-6 h-6 cursor-pointer"
+        on:click={() => forward(2, "Info")}
         on:keydown />
       <img
-        src="multimedia/Music_icon.png"
-        alt="music"
-        class="w-full h-full cursor-pointer"
-        on:click={() => forward(4, "Radio")}
+        src={dialogIconSrc}
+        alt="info"
+        class="w-6 h-6 cursor-pointer"
+        on:click={dialog}
         on:keydown />
     </div>
-    <div class="flex gap-2 p-2">
-      <img
-        src="multimedia/Notes_icon.png"
-        alt="notes"
-        class="w-full h-full cursor-pointer"
-        on:click={toggleJournal}
-        on:keydown />
-      <!-- drivers license hier? -->
-      <img
-        src="multimedia/Reviews_icon.png"
-        alt="notes"
-        class="w-full h-full cursor-pointer"
-        on:click={() => forward(2, "Reviews")}
-        on:keydown />
-      <img
-        src="multimedia/Settings_icon.png"
-        alt="settings"
-        class="w-full h-full cursor-pointer"
-        on:click={() => forward(5, "Settings")}
-        on:keydown />
+
+    <!-- this section -->
+    <div class="flex flex-col flex-1 justify-center">
+      <div class="flex gap-2 p-2">
+        <img
+          src="multimedia/Achievements_icon.png"
+          alt="achievements"
+          class="w-full h-full cursor-pointer"
+          on:click={() => forward(1, "Achievements")}
+          on:keydown />
+        <img
+          src="multimedia/Contacts_icon.png"
+          alt="contacts"
+          class="w-full h-full cursor-pointer"
+          on:click={() => forward(0, "Contacts")}
+          on:keydown />
+        <img
+          src="multimedia/Music_icon.png"
+          alt="music"
+          class="w-full h-full cursor-pointer"
+          on:click={() => forward(4, "Radio")}
+          on:keydown />
+      </div>
+      <div class="flex gap-2 p-2">
+        <img
+          src="multimedia/Notes_icon.png"
+          alt="notes"
+          class="w-full h-full cursor-pointer"
+          on:click={toggleJournal}
+          on:keydown />
+        <!-- drivers license hier? -->
+        <img
+          src="multimedia/Reviews_icon.png"
+          alt="notes"
+          class="w-full h-full cursor-pointer"
+          on:click={() => forward(2, "Reviews")}
+          on:keydown />
+        <img
+          src="multimedia/Settings_icon.png"
+          alt="settings"
+          class="w-full h-full cursor-pointer"
+          on:click={() => forward(5, "Settings")}
+          on:keydown />
+      </div>
     </div>
   </div>
 </div>
