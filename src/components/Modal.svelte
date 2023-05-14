5879e891771cc41468000a6014adab428eb33573
Renzo <21077703@student.hhs.nl>
2023-05-14 20:36:09 +0200

    fix max height of modal

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -17,7 +17,7 @@
     on:click|self
     class="fixed inset-0 z-50 bg-black/90 flex justify-center items-center pt-2">
     <div
-      class="bg-night-4 p-5 rounded max-h-80 xl:max-h-full w-8/12 max-w-screen-xl overflow-y-auto border-4 border-night-3">
+      class="bg-night-4 p-5 rounded max-h-80 xl:max-h-[35em] w-8/12 max-w-screen-xl overflow-y-auto border-4 border-night-3">
       {#if modalHeader}
         <div class="flex items-center justify-between">
           <p class="text-frost-1 text-4xl font-black">{modalHeader}</p>
