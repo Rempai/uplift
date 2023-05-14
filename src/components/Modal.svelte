b45ece1e9dcdc3d648a2d6b7cb59b239a8566caf
Renzo <21077703@student.hhs.nl>
2023-05-14 14:22:10 +0200

    make modal dynamic and header name optional

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -15,28 +15,28 @@
   <div
     on:keypress
     on:click|self
-    class="fixed inset-0 z-50 overflow-y-auto bg-black/90 flex justify-center items-center pt-2">
+    class="fixed inset-0 z-50 bg-black/90 flex justify-center items-center pt-2">
     <div
-      class="bg-night-4 p-5 rounded max-h-screen w-8/12 max-w-screen-xl overflow-y-auto border-4 border-night-3">
-      <div class="flex items-center justify-between">
-        <p class="text-frost-1 text-4xl font-black">{modalHeader}</p>
-        {#if closeButton}
-          <Button
-            onClick={() => {
-              dispatch("closed");
-              showModal = false;
-            }}
-            class="!shadow-transparent !p-1">
-            <div slot="icon">
-              <IoIosClose font-size="2em" class="text-aurora-red" />
-            </div>
-          </Button>
-        {/if}
-      </div>
-      <hr class="my-5" />
-      <div class="h-full overflow-y-auto">
-        <slot />
-      </div>
+      class="bg-night-4 p-5 rounded max-h-80 xl:max-h-full w-8/12 max-w-screen-xl overflow-y-auto border-4 border-night-3">
+      {#if modalHeader}
+        <div class="flex items-center justify-between">
+          <p class="text-frost-1 text-4xl font-black">{modalHeader}</p>
+          {#if closeButton}
+            <Button
+              onClick={() => {
+                dispatch("closed");
+                showModal = false;
+              }}
+              class="!shadow-transparent !p-1">
+              <div slot="icon">
+                <IoIosClose font-size="2em" class="text-aurora-red" />
+              </div>
+            </Button>
+          {/if}
+        </div>
+        <hr class="my-5" />
+      {/if}
+      <slot />
     </div>
   </div>
 {/if}
