0d92419e17d354a9db8491afe54b87cf341cb390
JFK <95449015+pro-jfk@users.noreply.github.com>
2023-06-07 16:49:20 +0200

    refactored and fixed positioning

--- src/components/Arrow.svelte
+++ src/components/Arrow.svelte
@@ -1,64 +1,67 @@
 <script lang="ts">
-  import { component_subscribe, element_is } from "svelte/internal";
   import BSArrow from "~icons/bxs/down-arrow";
 
   export let targetElement;
   export let showArrow: boolean;
 
   // predefined elements of buttons in multimedia
-  let achievementsElement = document.querySelector('img[alt="achievements"]');
-  let contactsElement = document.querySelector('img[alt="contacts"]');
-  let musicElement = document.querySelector('img[alt="music"]');
-  let notesElement = document.querySelector('img[alt="notes"]');
-  let reviewsElement = document.querySelector('img[alt="reviews"]');
-  let settingsElement = document.querySelector('img[alt="settings"]');
-  let licenseElement = document.querySelector(".diverLicense");
+  let elements = {
+    achievementsElement: document.querySelector('img[alt="achievements"]'),
+    contactsElement: document.querySelector('img[alt="contacts"]'),
+    musicElement: document.querySelector('img[alt="music"]'),
+    notesElement: document.querySelector('img[alt="notes"]'),
+    reviewsElement: document.querySelector('img[alt="reviews"]'),
+    settingsElement: document.querySelector('img[alt="settings"]'),
+    licenseElement: document.getElementById("diver"),
+  };
 
   let left;
   let top;
-  let width;
+  let width, height;
+  let x, y;
 
-  async function getRoundedBoundingClientRect(targetElement) {
-    const props = await targetElement.getBoundingClientRect();
-    left = Math.round(props.left);
-    top = Math.round(props.top);
-    width = Math.round(props.width);
+  function getElementPosition(targetElement) {
+    const props = targetElement.getClientRects();
+    left = Math.round(props[0].left);
+    top = Math.round(props[0].top);
+    x = Math.round(props[0].x);
+    y = Math.round(props[0].y);
+    height = Math.round(props[0].height);
+    width = Math.round(props[0].width);
   }
 
   $: if (targetElement && targetElement.content.includes("clicking the contacts button")) {
     showArrow = true;
-    getRoundedBoundingClientRect(contactsElement);
   } else if (targetElement.content.includes("Try opening the journal")) {
-    getRoundedBoundingClientRect(notesElement);
-  } else if (targetElement.content.includes("purpose we have the review section")) {
-    getRoundedBoundingClientRect(reviewsElement);
+    getElementPosition(elements.notesElement);
+  } else if (targetElement.content.includes("the review section")) {
+    getElementPosition(elements.reviewsElement);
     /*TODO:
   Fix:Arrow isn't aligning properly on the icon of the driver license, need to fix.
 */
   } else if (targetElement.content.includes("Try opening your license")) {
-    getRoundedBoundingClientRect(licenseElement);
+    getElementPosition(elements.licenseElement);
   } else if (targetElement.content.includes("by opening the list of achievements")) {
-    getRoundedBoundingClientRect(achievementsElement);
+    getElementPosition(elements.achievementsElement);
   } else if (targetElement.content.includes("Try opening your radio menu")) {
-    getRoundedBoundingClientRect(musicElement);
+    getElementPosition(elements.musicElement);
   } else if (targetElement.content.includes("Try opening your settings")) {
-    getRoundedBoundingClientRect(settingsElement);
+    getElementPosition(elements.settingsElement);
   }
-
-  // const props = targetElement.getBoundingClientRect();
-  // let left = Math.round(props.left);
-  // let top = Math.round(props.top);
-  // let width = Math.round(props.width);
 
   /*TODO:
 Fix:Does not work with custom components, only DOM elements
 Need to come up with better ideas for tutorial flow, if this is too difficult.
 Probably need to hardcode x,y coordinates for arrow.
 */
+  $: console.log(elements.contactsElement.getClientRects());
 </script>
 
 {#if showArrow}
-  <div class="arrow absolute z-20" style:top="{top / 1.1}px" style:left="{left}px">
-    <BSArrow class="text-aurora-orange/100 brightness-125 animate-bounce" font-size="3em" />
+  <div
+    class="absolute z-20 animate-bounce"
+    style:left="{left + width / 16}px"
+    style:top="{top - height}px">
+    <BSArrow class="text-aurora-orange/100 brightness-125" font-size="3em" />
   </div>
 {/if}
