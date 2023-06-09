5c676db2cfc67e33ae3580b6a7a127b01f57f4c2
luuks1609 <luuks1609@gmail.com>
2023-06-09 12:26:35 +0200

    tussenproduc t

--- src/components/Notification.svelte
+++ src/components/Notification.svelte
@@ -17,16 +17,46 @@
 </script>
 
 <div class="absolute top-10 right-10 z-50 flex flex-col items-end space-y-4">
-  {#each messages as msg, i}
-    <button
-      class={`hover:brightness-110 rounded py-2 px-3 w-48 cursor-pointer shadow ${
-        info ? "bg-frost-4" : "bg-aurora-red"
-      }`}
-      on:click={() => removemessages(i)}>
-      <div class="flex justify-between gap-2">
+  {#each messages as msg, index}
+    <div
+      class="relative message-container hover:brightness-110 rounded py-2 px-3 w-48 cursor-pointer shadow bg-aurora-red">
+      <div class="message animate-fade-out" on:animationend={() => removemessages(index)}>
         <p class="break-words">{msg}</p>
-        <span class="">&times;</span>
       </div>
-    </button>
+      <div class="progress-bar animate-progress-bar" />
+    </div>
   {/each}
 </div>
+
+<style>
+  @keyframes fade-out {
+    0% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+
+  @keyframes progress-bar {
+    0% {
+      width: 0%;
+    }
+    100% {
+      width: 100%;
+    }
+  }
+
+  .message-container {
+    position: relative;
+  }
+
+  .progress-bar {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 5px;
+    background-color: #4caf50;
+    animation: progress-bar 5s linear forwards;
+  }
+</style>
