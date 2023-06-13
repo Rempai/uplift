d29419e55d5cd5069c3e22f6ece69c4985a91106
Periko_11 <22068023@student.hhs.nl>
2023-06-13 11:32:36 +0200

    -error message(s) admin login

--- src/routes/AdminLogin.svelte
+++ src/routes/AdminLogin.svelte
@@ -2,31 +2,56 @@
   import { push } from "svelte-spa-router";
 
   import { parseJwt } from "@/lib/jwtParser";
-  import { validation } from "@/lib/stores";
-  import { validationErrorCheck } from "@/lib/validation";
   import { loginForAccessToken } from "@/lib/authProcesses";
 
   import Form from "@/components/Form.svelte";
   import Button from "@/components/Button.svelte";
+  import Notification from "@/components/Notification.svelte";
 
-  $validation.length = 0;
-  const handleSubmit = async ({ target }) => {
-    const login = await loginForAccessToken(target);
-    if (login == true) {
-      let parsedJwt = await parseJwt(localStorage.getItem("access_token"));
-      if (parsedJwt.role === "Admin" || parsedJwt.role === "Writer") {
-        push("/admin");
-      } else {
-        // TODO: show error that user is not admin
-        push("/");
+  let messages: Array<string> = [];
+
+  const showError = (err: string) => {
+    messages = [...messages, err];
+  };
+
+  const ErrorMessage = (str: string) => {
+    function isJsonString(str: string) {
+      try {
+        JSON.parse(str);
+      } catch {
+        return false;
       }
-    } else {
-      validationErrorCheck(login, false);
+      return true;
     }
 
-    if (localStorage.getItem("access_token") === null) {
-      return;
+    if (typeof (str === "object")) {
+      // @ts-ignore
+      const res = Object.values(str)[1].value;
+
+      if (isJsonString(res)) {
+        const obj = JSON.parse(res);
+        return obj[1].message;
+      }
+      return str;
     }
+  };
+
+  const handleSubmit = async ({ target }) => {
+    const login = await loginForAccessToken(target)
+      .then((login) => {
+        return parseJwt(login.access_token);
+      })
+      .then((parsedJwt) => {
+        if (parsedJwt.role === "Admin" || parsedJwt.role === "Writer") {
+          push("/admin");
+        } else {
+          showError("User doesn't have admin or writer role.");
+          push("/");
+        }
+      })
+      .catch((err) => {
+        showError(ErrorMessage(err));
+      });
   };
 </script>
 
@@ -36,6 +61,7 @@
 
 <main
   class="w-full h-screen flex items-center justify-center bg-[url('/bg.png')] bg-cover bg-center">
+  <Notification {messages} />
   <div class="flex flex-col items-center w-fit p-6 md:px-48 rounded bg-night-2/80 shadow">
     <h1>Login</h1>
     <Form login={true} {handleSubmit} />
