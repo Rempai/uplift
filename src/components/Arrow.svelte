81698916b7eaf06106ca699d74243a2f776cc026
JFK <95449015+pro-jfk@users.noreply.github.com>
2023-06-20 13:01:39 +0200

    added not clickable

--- src/components/Arrow.svelte
+++ src/components/Arrow.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
-  import { onDestroy, onMount, tick } from "svelte";
-  import { rideQuit, rendered } from "@/lib/stores";
+  //@ts-nocheck
+  import { onDestroy, onMount } from "svelte";
+  import { rideQuit, tutorialElements, tutorialImageElements } from "@/lib/stores";
   import BSArrow from "~icons/bxs/down-arrow";
 
   export let passage;
 
   let showArrow = false;
-  let elements = {};
-  let element;
+  let buttonElements = {};
+  let imgElements = {};
+  let lastElement;
 
   let arrowWidth;
   let left = 0;
@@ -16,37 +18,59 @@
   let height = 0;
 
   onMount(() => {
-    elements = {
-      contacts: document.querySelector('img[alt="contacts"]'),
-      notes: document.querySelector('img[alt="notes"]'),
-      reviews: document.querySelector('img[alt="reviews"]'),
-      license: document.getElementById("diverLicense"),
-      achievements: document.querySelector('img[alt="achievements"]'),
-      music: document.querySelector('img[alt="music"]'),
-      settings: document.querySelector('img[alt="settings"]'),
-      progress: document.querySelector(".progresstut"),
+    buttonElements = {
+      rides: document.getElementById("rides"),
+      journal: document.getElementById("journal"),
+      reviews: document.getElementById("reviews"),
+      driversLicense: document.getElementById("driversLicense"),
+      achievements: document.getElementById("achievements"),
+      radio: document.getElementById("radio"),
+      settings: document.getElementById("settings"),
+      progress: document.getElementById("storyProgress"),
     };
 
-    for (let key in elements) {
-      const element = elements[key];
-      if (key != "journal") {
-        element.classList.add("cursor-not-allowed", "brightness-50");
-      }
+    imgElements = {
+      rides: document.querySelector('img[alt="contacts"]'),
+      journal: document.querySelector('img[alt="notes"]'),
+      reviews: document.querySelector('img[alt="reviews"]'),
+      driversLicense: document.getElementById("driversLicenseIcon"),
+      achievements: document.querySelector('img[alt="achievements"]'),
+      radio: document.querySelector('img[alt="music"]'),
+      settings: document.querySelector('img[alt="settings"]'),
+    };
+
+    // for (let key in buttonElements) {
+    //   const buttonElement = buttonElements[key];
+
+    //   if (key != "settings" && key != "radio") {
+    //     buttonElement.classList.add("pointer-events-none");
+    //   }
+    // }
+
+    for (let key in imgElements) {
+      const imgElement = imgElements[key];
+      imgElement.classList.add("brightness-50");
     }
+    $tutorialElements = buttonElements;
+    $tutorialImageElements = imgElements;
   });
 
   function getElementPosition(targetElement) {
     const props = targetElement.getClientRects();
     left = Math.round(props[0].left);
     top = Math.round(props[0].top);
+    if (targetElement.id === "driversLicense") {
+      top -= 20;
+    }
     height = Math.round(props[0].height);
     width = Math.round(props[0].width);
     showArrow = true;
   }
 
-  const restoreClasses = () => {
-    for (let key in elements) {
-      let element = elements[key];
+  const hideElement = () => {
+    for (let key in $tutorialImageElements) {
+      let element = $tutorialImageElements[key];
+
       element.classList.remove(
         "border",
         "border-4",
@@ -54,30 +78,61 @@
         "rounded",
         "outline-offset-2"
       );
-      element.classList.add("cursor-not-allowed", "brightness-50");
+      element.classList.add("brightness-50");
     }
+
+    // for (let key in $tutorialElements) {
+    //   let element = $tutorialElements[key];
+    //   element.classList.add("pointer-events-none");
+    // }
     showArrow = false;
   };
 
-  const changeClasses = (currentElement) => {
-    currentElement.classList.remove("cursor-not-allowed", "brightness-50");
-
-    currentElement.classList.add(
-      "border",
-      "border-4",
-      "border-aurora-yellow/70",
-      "rounded",
-      "outline-offset-2"
-    );
-    element = currentElement;
+  const highlightElement = (currentElement) => {
+    if (currentElement.id !== "storyProgress") {
+      const imgElement = $tutorialImageElements[currentElement.id];
+      currentElement.classList.remove("pointer-events-none");
+      imgElement.classList.remove("brightness-50");
+      imgElement.classList.add(
+        "border",
+        "border-4",
+        "border-aurora-yellow/70",
+        "rounded",
+        "outline-offset-2"
+      );
+      lastElement = imgElement;
+    } else {
+      currentElement.classList.remove("pointer-events-none");
+      currentElement.classList.add(
+        "border",
+        "border-4",
+        "border-aurora-yellow/70",
+        "rounded",
+        "outline-offset-2"
+      );
+      lastElement = currentElement;
+    }
   };
 
   // Remove classes after tutorial is done
   function removeClasses() {
-    for (let key in elements) {
-      const element = elements[key];
+    for (let key in $tutorialElements) {
+      const element = $tutorialElements[key];
+
+      element.classList.remove("pointer-events-none");
+    }
+
+    for (let key in $tutorialImageElements) {
+      const element = $tutorialImageElements[key];
       if (key !== "journal") {
-        element.classList.remove("cursor-not-allowed", "brightness-50");
+        element.classList.remove(
+          "border",
+          "border-4",
+          "border-aurora-yellow/70",
+          "rounded",
+          "outline-offset-2",
+          "brightness-50"
+        );
       }
     }
   }
@@ -100,24 +155,22 @@
       passage.content.includes("You are almost finished") ||
       passage.content.includes("Now that you have finished this branch")
     ) {
-      restoreClasses();
+      hideElement();
     } else {
       const foundPassage = Object.keys(keyPassages).find((key) => passage.content.includes(key));
-
-      if (foundPassage !== undefined && $rendered === false) {
-        getElementPosition(Object.values(elements)[keyPassages[foundPassage]]);
-        changeClasses(Object.values(elements)[keyPassages[foundPassage]]);
+      if (foundPassage !== undefined) {
+        getElementPosition(Object.values($tutorialElements)[keyPassages[foundPassage]]);
+        highlightElement(Object.values($tutorialElements)[keyPassages[foundPassage]]);
       }
     }
   }
 
   onDestroy(() => {
-    tick();
     if ($rideQuit !== true) {
-      restoreClasses();
+      hideElement();
     } else {
-      if (element) {
-        element.classList.remove(
+      if (lastElement) {
+        lastElement.classList.remove(
           "border",
           "border-4",
           "border-aurora-yellow/70",
